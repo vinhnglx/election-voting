b60b1fc53f7c03d8e882db4212bd5b26480160f7
Vincent <vinh.nglx@gmail.com>
2018-08-07 14:39:26 +0800

    Implement vote function for Election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,9 @@ contract Election {
     uint voteCount;
   }
 
+  // Store account that have voted
+  mapping(address => bool) public voters;
+
   // Store Candidate
   // Fetch Candidate
   mapping(uint => Candidate) public candidates;
@@ -18,6 +21,23 @@ contract Election {
   constructor() public {
     addCandidate("Luffy");
     addCandidate("Zoro");
+  }
+
+  function vote (uint _candidateId) public {
+    // Require that they haven't voted before
+    // require() to validate user's input before any action can be taken
+    // Stop execution and throw exception if false
+    require(!voters[msg.sender]);
+
+    // Require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+    // Record that voter has voted
+    // msg.sender is the address currently interacting with the contract or is the one who is sending transaction
+    voters[msg.sender] = true;
+
+    // Increate candidate voteCount
+    candidates[_candidateId].voteCount++;
   }
 
   function addCandidate (string _name) private {
--- test/election.js
+++ test/election.js
@@ -24,4 +24,50 @@ contract('Election', function (accounts) {
       assert.equal(candidate[2], 0)
     })
   })
+
+  it('allowed a voter to do a vote', function () {
+    return Election.deployed().then(function (instance) {
+      electionInstance = instance
+      candidateId = 1
+      return electionInstance.vote(candidateId, { from: accounts[0] })
+    }).then(function (receipt) {
+      return electionInstance.voters(accounts[0])
+    }).then(function (voted) {
+      assert(voted, 'the voter was marked as voted')
+      return electionInstance.candidates(candidateId)
+    }).then(function (candidate) {
+      var voteCount = candidate[2]
+      assert.equal(voteCount, 1)
+    })
+  })
+
+  it('throws an exception for invalid candidates', function () {
+    return Election.deployed().then(function (instance) {
+      electionInstance = instance
+      return electionInstance.vote(9999, { from: accounts[1] })
+    }).then(assert.fail).catch(function (err) {
+      assert(err.message.indexOf('revert') >= 0)
+      return electionInstance.candidates(1)
+    }).then(function (candidate1) {
+      assert.equal(candidate1[2], 1)
+      return electionInstance.candidates(2)
+    }).then(function (candidate2) {
+      assert.equal(candidate2[2], 0)
+    })
+  })
+
+  it('throws an exception for double voting', function () {
+    return Election.deployed().then(function (instance) {
+      electionInstance = instance
+      candidateId = 2
+      electionInstance.vote(candidateId, { from: accounts[1] })
+      return electionInstance.candidates(2)
+    }).then(function (candidate2) {
+      var voteCount = candidate2[2]
+      assert.equal(voteCount, 1)
+      return electionInstance.vote(candidateId, { from: accounts[1] })
+    }).then(assert.fail).catch(function (err) {
+      assert(err.message.indexOf('revert') >= 0)
+    })
+  })
 })
